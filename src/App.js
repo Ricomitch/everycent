9c20becd5abce1923296afe7d0fd1576da06350b
Rico <ricomitch@icloud.com>
2020-08-04 16:32:00 -0400

    moved expenses to separate component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Route } from "react-router-dom"
 import './App.css';
 import axios from 'axios'
 import Data from "./Data"
+import CreateExpenses from "./CreateExpenses"
 
 function App() {
   const [funds, updateFunds] = useState([])
-  const [category, updateCategory] = useState('')
-  const [item, updateItem] = useState('')
-  const [amount, updateAmount] = useState('')
+  
   const [fetchExpenses, updateFetchExpenses] = useState(false)
     
   useEffect(() => {
@@ -23,45 +22,16 @@ function App() {
     apiCall()
   }, [fetchExpenses])
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const data = await axios.post("https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201", {
-        fields: {
-          "Category": category,
-          "Items": item,
-          "Amount": parseInt(amount,10)
-        }
-    }, {
-      
-      headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    })
-    updateFetchExpenses(!fetchExpenses)
-    updateCategory('')
-    updateItem('')
-    updateAmount('')
-  }
 
   return (
     <main>
       <h1>My Funds</h1>
+      <CreateExpenses
+        updateFetchExpenses={updateFetchExpenses}
+        fetchExpenses={fetchExpenses}
+      />
       {funds.map(data => <Data data={data} key={data.id} />)}
-      <h2>Add Expenses</h2>
-      <form onSubmit={handleSubmit}>
-
-        <label htmlFor="category">Category</label>
-        <input type="text" id="category" onChange={e => updateCategory(e.target.value)} value={category}/>
-
-        <label htmlFor="item">Item</label>
-        <input type="text" id="item" onChange={e => updateItem(e.target.value)} value={item} />
-
-        <label htmlFor="amount">Amount</label>
-        <input type="text" id="amount" onChange={e => updateAmount(e.target.value)} value={amount} />
-        <input type="submit" value="Submit"/>
-         
-      </form>
+      
     </main>)
   // <div>
   //   <Route path="/" exact>
---
+++ src/CreateExpenses.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react"
+import axios from "axios"
+
+function CreateExpense(props) {
+
+  const [category, updateCategory] = useState('')
+  const [item, updateItem] = useState('')
+  const [amount, updateAmount] = useState('')
+    
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = await axios.post("https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201", {
+        fields: {
+          "Category": category,
+          "Items": item,
+          "Amount": parseInt(amount,10)
+        }
+    }, {
+      
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    props.updateFetchExpenses(!props.fetchExpenses)
+    updateCategory('')
+    updateItem('')
+    updateAmount('')
+  }  
+    
+    
+  return <form onSubmit={handleSubmit}>
+        <h2>Add Expenses</h2>
+        <label htmlFor="category">Category</label>
+        <input type="text" id="category" onChange={e => updateCategory(e.target.value)} value={category}/>
+
+        <label htmlFor="item">Item</label>
+        <input type="text" id="item" onChange={e => updateItem(e.target.value)} value={item} />
+
+        <label htmlFor="amount">Amount</label>
+        <input type="text" id="amount" onChange={e => updateAmount(e.target.value)} value={amount} />
+        <input type="submit" value="Submit"/>
+         
+      </form>
+}
+
+export default CreateExpense
