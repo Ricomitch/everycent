23ce6e9e3c737b1a55ca4ec1a0db4151b53ba46c
Rico <ricomitch@icloud.com>
2020-08-05 13:30:16 -0400

    added route and balance component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import { Route } from "react-router-dom"
-import './App.css';
-import axios from 'axios'
-import Data from "./components/Data"
-import CreateExpenses from "./components/CreateExpenses"
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import axios from "axios";
+import Data from "./components/Data";
+import CreateExpenses from "./components/CreateExpenses";
+import { Nav } from "./components/Nav";
+import UpdateExpense from "./components/UpdateExpense";
+import Balance from "./components/Balance";
 
 function App() {
-  const [funds, updateFunds] = useState([])
-  
-  const [fetchExpenses, updateFetchExpenses] = useState(false)
-    
+  const [funds, updateFunds] = useState([]);
+  const [balance, updateBalance] = useState(0)
+  const [fetchExpenses, updateFetchExpenses] = useState(false);
+
   useEffect(() => {
     const apiCall = async () => {
-      const data = await axios.get("https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201?view=Grid%20view", {
-        headers: {
-          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+      const data = await axios.get(
+        "https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201?view=Grid%20view",
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+          },
         }
-      }) 
-      updateFunds(data.data.records)      
-    }
-    apiCall()
-  }, [fetchExpenses])
+      );
+      updateFunds(data.data.records);
+    };
+    apiCall();
+  }, [fetchExpenses]);
 
+  useEffect(() => {
+    let total = 0
+    if (funds.length > 0) {
+      funds.forEach(element => {
+       total += element.fields.Amount 
+      });
+
+      updateBalance(total)
+    }
+  }, [funds])
 
   return (
-    <main>
-      <h1>My Funds</h1>
-      <CreateExpenses
-        updateFetchExpenses={updateFetchExpenses}
+    <div>
+      <main>
+        <div>
+          <Nav />
+          <Balance balance={balance}/>
+        </div>
+        <h2>My Funds</h2>
+        {/* <CreateExpenses */}
+        {/* updateFetchExpenses={updateFetchExpenses}
         fetchExpenses={fetchExpenses}
-      />
-      {funds.map(data => <Data data={data} key={data.id} fetchExpenses={fetchExpenses} updateFetchExpenses={updateFetchExpenses}  />)}
-      
-    </main>)
-  // <div>
-  //   <Route path="/" exact>
-  //     <h2>Category</h2>
-  //     <h2>Expenses</h2>
-  //   </Route>
-  //   <Route path="/add">
-  //     <h2>Add Expenses</h2>
-  //   </Route>
-  // </div>
-  
+      /> */}
+        {/* {funds.map(data => <Data data={data} key={data.id} fetchExpenses={fetchExpenses} updateFetchExpenses={updateFetchExpenses} />)} */}
+      </main>
+
+      <Route path="/" exact>
+        {funds.map((data) => (
+          <Data
+            data={data}
+            key={data.id}
+            fetchExpenses={fetchExpenses}
+            updateFetchExpenses={updateFetchExpenses}
+          />
+        ))}
+
+        {/* <UpdateExpense
+        category={category}
+        updatecategory={updateCategory}
+        items={items}
+        updateitems={updateItems}
+        amount={amount}
+        updateamount={updateAmount}
+      />    */}
+      </Route>
+
+      <Route path="/add" exact>
+        <CreateExpenses
+          updateFetchExpenses={updateFetchExpenses}
+          fetchExpenses={fetchExpenses}
+         
+        />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <h1>
+      Every Cent
+    </h1>
+  )
+}
+
+export default Nav
