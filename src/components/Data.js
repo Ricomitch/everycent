14eecff2514fccf27d6897e003c6872f536d47d2
Rico <ricomitch@icloud.com>
2020-08-04 20:21:44 -0400

    setup delete component

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,10 +1,22 @@
 import React from "react";
 import UpdateExpense from "./UpdateExpense";
+import axios from "axios"
 
 function Data(props) {
   const data = props.data;
+
+  const deleteData = async () => {
+    const data = await axios.delete(`https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201/${data.id}`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+      }
+    })
+
+  }
+
   return (
     <div>
+      <button onClick={deleteData}>Delete</button>
       <h2>{data.fields.Category}</h2>
       <h4>{data.fields.Items}</h4>
       <h3>{data.fields.Amount}</h3>
