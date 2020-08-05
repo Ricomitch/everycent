d813869b47fd07db4ecc41ddea0c100af458cc1b
Rico <ricomitch@icloud.com>
2020-08-05 00:23:36 -0400

    added delete button

--- src/components/Data.js
+++ src/components/Data.js
@@ -6,21 +6,21 @@ function Data(props) {
   const data = props.data;
 
   const deleteData = async () => {
-    const data = await axios.delete(`https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201/${data.id}`, {
+    const response = await axios.delete(`https://api.airtable.com/v0/appqyiVOvBv6WBtX3/Table%201/${data.id}`, {
       headers: {
         'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
       }
     })
-
+  props.updateFetchExpenses(!props.fetchExpenses)
   }
 
   return (
     <div>
-      <button onClick={deleteData}>Delete</button>
       <h2>{data.fields.Category}</h2>
       <h4>{data.fields.Items}</h4>
       <h3>{data.fields.Amount}</h3>
       <UpdateExpense data={data} fetchExpenses={props.fetchExpenses} updateFetchExpenses={props.updateFetchExpenses} />
+      <button onClick={deleteData}>Delete</button>
     </div>
   );
 }
